b068b57f14039a4a4a48e928532562b8a1963719
zuiwuchang <zuiwuchang@gmail.com>
2022-09-03 15:40:36 +0800

    version

--- version/version.go
+++ version/version.go
@@ -2,6 +2,6 @@ package version
 
 var (
 	Version = `v0.0.1`
-	Commit = `b465a76662297afb3a715c8264dcc2212206a310`
-	Date = `2022-09-02 10:59:18`
+	Commit = `a1c52783963bc789a382e0e640f6f3db6294ab5b`
+	Date = `2022-09-03 15:40:28`
 )
